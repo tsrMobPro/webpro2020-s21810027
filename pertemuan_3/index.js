19278d3ac5c8322685a35cd1b5e424361a1cec8c
tsrMobPro <s21810027@student.unklab.ac.id>
2020-08-26 19:19:34 +0800

    Update baru

---
+++ pertemuan_3/index.js
@@ -0,0 +1,19 @@
+/* 
+    1. Variabel dan tipe data
+*/
+
+const firstName = "John";
+const lastName = "Smith";
+let age = 20;
+age = 21;
+const isMarried = true;
+
+console.log("My name is", firstName, lastName + ". I'm", age, "years old.");
+
+console.log(typeof isMarried);
+
+// 2. Operator
+
+let bilangan = 10;
+
+console.log(bilangan === "10");
